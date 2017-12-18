7aeb274a0ca3896ccd227c3724ed1a1c96ba88fe
Jay Barra <jay.barra@phoenix-opsgroup.com>
2017-12-18 13:44:12 -0500

    added catch logic for page not found issues for the UI added login page

--- src/js/App.js
+++ src/js/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from "react";
 import PropTypes from "prop-types";
-import {Link, Route} from "react-router-dom";
+import {Link, Route, Switch, Redirect} from "react-router-dom";
 import {Container, Menu, Button} from "semantic-ui-react";
 
 import {Home, Characters, Campaigns, Login} from "./components";
@@ -40,18 +40,27 @@ export class App extends Component {
         const {location} = this.props;
         const {visible} = this.state;
 
+        let hideMenuStates = ["/", "/login"];
+
+        const topMenuBar = ((visible && location.pathname === "/") || !hideMenuStates.includes(location.pathname)) ?
+            <FixedMenu hideLogin={location.pathname !== "/"}/> : null;
+
         return (
             <div className="app-wrapper">
-                {(visible && location.pathname === "/") || location.pathname !== "/" ?
-                    <FixedMenu hideLogin={location.pathname !== "/"}/> : null}
+                {topMenuBar}
 
-                <Route exact
-                       path="/"
-                       render={() => <Home hideMenu={this.hideFixedMenu} showMenu={this.showFixedMenu}/>}/>
+                <Switch>
+                    <Route exact
+                           path="/"
+                           render={() => <Home hideMenu={this.hideFixedMenu} showMenu={this.showFixedMenu}/>}/>
 
-                <Route path="/campaigns" component={Campaigns}/>
-                <Route path="/characters" component={Characters}/>
-                <Route path="/login" component={Login}/>
+                    <Route path="/campaigns" component={Campaigns}/>
+                    <Route path="/characters" component={Characters}/>
+                    <Route path="/login" component={Login}/>
+
+                    {/** Catch 404s and redirect to home for now */}
+                    <Redirect to="/"/>
+                </Switch>
 
             </div>
         );
--- src/js/components/Login/Login.js
+++ src/js/components/Login/Login.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {Link} from "react-router-dom";
 import {Grid, Header, Button, Form} from "semantic-ui-react";
 
 export class Login extends React.Component {
@@ -14,24 +15,23 @@ export class Login extends React.Component {
 
     render() {
         return (
-            <Grid container>
-                <Grid.Row>
-                    <Grid.Column>
-                        <Header as="h1"
-                                content={"Login"}
-                        />
-                    </Grid.Column>
-                </Grid.Row>
-
-                <Grid.Row>
-                    <Grid.Column>
-                        <Form>
-                            <Form.Input placeholder="Username"/>
-                            <Form.Input type="password" placeholder="Password"/>
-                            <Button primary type="button" onClick={() => this.handleLogin()}>Login</Button>
-                        </Form>
-                    </Grid.Column>
-                </Grid.Row>
+            <Grid centered>
+                <Grid.Column width={4}>
+                    <Header as="h1"
+                            content={"Login"}
+                    />
+                    <Form>
+                        <Form.Input name="username" placeholder="Username"/>
+                        <Form.Input name="password" type="password" placeholder="Password"/>
+                        <Button.Group fluid>
+                            <Button as={Link} to="/signup" secondary type="button">Sign Up</Button>
+                            <Button primary
+                                    type="button"
+                                    floated="right"
+                                    onClick={() => this.handleLogin()}>Login</Button>
+                        </Button.Group>
+                    </Form>
+                </Grid.Column>
             </Grid>
         );
     }
